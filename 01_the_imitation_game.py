7f103d2a7359d0b5cefd7ff46e530941f189fccd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-10-21 23:49:14 +0200

    Update 01_the_imitation_game.py

--- 01_the_imitation_game.py
+++ 01_the_imitation_game.py
@@ -1,20 +1,19 @@
-# Initialize the encrypted message
+
 encrypted_message = input()
 
-# Loop to read and process each command
 while True:
-    # Read a command
+    
     command_line = input()
 
-    # If "Decode" is received, break the loop
+    
     if command_line == "Decode":
         break
 
-    # Split the command into its components
+   
     parts = command_line.split("|")
     command = parts[0]
 
-    # Perform the appropriate action based on the command
+    
     if command == "Move":
         n = int(parts[1])
         encrypted_message = encrypted_message[n:] + encrypted_message[:n]
@@ -27,5 +26,5 @@ while True:
         replacement = parts[2]
         encrypted_message = encrypted_message.replace(substring, replacement)
 
-# Print the decrypted message
+
 print(f"The decrypted message is: {encrypted_message}")
